d2b172294abad7c8b5765957ac9cb957327d1b3c
sadranoras <norassadra1388@gmail.com>
2025-07-25 12:40:07 +0330

    Update server.js

--- server.js
+++ server.js
@@ -205,7 +205,8 @@ app.post('/change-password', requireLogin, async (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`سرور روی http://localhost:${PORT} اجرا شده`);
+  console.log(`Server running on port ${PORT}`);
 });
+
